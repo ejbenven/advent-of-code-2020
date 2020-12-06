44d83044b995e57d6b2da787e1e34ed8cab0ba16
Eloi Benvenuti <me@eloi.io>
2020-12-06 23:50:13 +0100

    Day 4 Part 2

--- day4/sol.cpp
+++ day4/sol.cpp
@@ -2,7 +2,9 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <unordered_set>
 #include <unordered_map>
+#include <regex>
 
 #include "absl/strings/str_split.h"
 
@@ -11,8 +13,10 @@ using std::cout;
 using std::endl;
 using std::vector;
 using std::unordered_map;
+using std::stoul;
 
-bool validate(vector<string> lines);
+bool validate1(vector<string> lines);
+bool validate2(vector<string> lines);
 unordered_map<string, string> parseInput(vector<string> lines);
 
 int main() {
@@ -25,7 +29,8 @@ int main() {
     auto validCnt = 0;
     while(std::getline(input, line)) {
         if (line == "" and lines.size() != 0) {
-            if(validate(lines)) {
+            //if(validate1(lines)) {
+            if(validate2(lines)) {
                 validCnt += 1;
             }
             lines.clear();
@@ -34,7 +39,8 @@ int main() {
         }
     }
     if(not (lines.size() == 0)) {
-        if (validate(lines)) {
+        //if (validate1(lines)) {
+        if (validate2(lines)) {
             validCnt += 1;
         }
     }
@@ -43,7 +49,73 @@ int main() {
 
 }
 
-bool validate(vector<string> lines)
+bool validate2(vector<string> lines)
+{
+    vector<string> mandatoryFields = {"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid", };
+    vector<string> optionalFields = { "cid", };
+
+    auto keys = parseInput(lines);
+    for (auto field : mandatoryFields) {
+        if (not keys.contains(field)) {
+            return false;
+        }
+    }
+
+    for (const auto& item : keys) {
+        if (item.first == "byr") {
+            if (item.second.size() != 4 or (stoul(item.second) < 1920 or stoul(item.second) > 2002)) {
+                return false;
+            }
+        } else if (item.first == "iyr") {
+            if (item.second.size() != 4 or (stoul(item.second) < 2010 or stoul(item.second) > 2020)) {
+                return false;
+            }
+        } else if (item.first == "eyr") {
+            if (item.second.size() != 4 or (stoul(item.second) < 2020 or stoul(item.second) > 2030)) {
+                return false;
+            }
+        } else if (item.first == "hgt") {
+            if (item.second.size() < 4) {
+                return false;
+            }
+            auto unitLength = 2;
+            auto unit = item.second.substr(item.second.size() - unitLength, unitLength);
+            auto data = stoul(item.second.substr(0, item.second.size() - unitLength));
+            if (unit == "cm") {
+                if (data < 150 or data > 193) {
+                    return false;
+                }
+            } else if (unit == "in") {
+                if (data < 59 or data > 76) {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        } else if (item.first == "hcl") {
+            std::smatch base_match{};
+            if (not std::regex_match(item.second, base_match, std::regex("^#[a-f_0-9]{6}$"))) {
+                return false;
+            }
+        } else if (item.first == "ecl") {
+            std::unordered_set<string> eyeColors = {"amb", "blu", "brn", "gry", "grn", "hzl", "oth",};
+            if (not eyeColors.contains(item.second)) {
+                return false;
+            }
+        } else if (item.first == "pid") {
+            std::smatch base_match{};
+            if (not std::regex_match(item.second, base_match, std::regex("^[0-9]{9}$"))) {
+                return false;
+            }
+        }else if (item.first != "cid") {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+bool validate1(vector<string> lines)
 {
     vector<string> mandatoryFields = {"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid", };
     vector<string> optionalFields = { "cid", };
