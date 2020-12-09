644466b1102037125625e987ded45cbc4b0181ff
Eloi Benvenuti <me@eloi.io>
2020-12-09 22:36:07 +0100

    Day6 Part 1

---
+++ day6/sol.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <unordered_set>
+
+using std::string;
+using std::cout;
+using std::endl;
+using std::unordered_set;
+
+unordered_set<char> parse(string line);
+
+int main() {
+    std::ifstream input;
+    input.open("day6/input.txt");
+
+
+    string line;
+    string group = "";
+    auto counts = 0;
+    while(std::getline(input, line)) {
+        if(line == "" and group.size() != 0) {
+            auto ans = parse(group);
+            counts += ans.size();
+            group = "";
+        } else {
+            group += line;
+        }
+    }
+
+    if (group != "") {
+        auto ans = parse(group);
+        counts += ans.size();
+        group = "";
+    }
+
+    cout << "sum: " << counts << endl;
+
+    return 0;
+}
+
+unordered_set<char> parse(string line)
+{
+    unordered_set<char> ans{};
+
+    for (auto c : line) {
+        ans.insert(c);
+    }
+
+    return ans;
+}
+
