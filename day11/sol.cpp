e3ad0358f5b87cdf36c127f696023f0ef4b62b21
Eloi Benvenuti <me@eloi.io>
2020-12-12 23:50:06 +0100

    Day 11 Part 2

--- day11/sol.cpp
+++ day11/sol.cpp
@@ -14,9 +14,91 @@ enum state_t {
     OCCUPIED,
 };
 
+class UpdateRules {
+    public:
+        virtual ~UpdateRules()=default;
+        virtual void update(vector<vector<state_t>> & newWorld, const vector<vector<state_t>> & prevWorld) const=0;
+};
+
+class UpdateRules1 : public UpdateRules {
+    public:
+        void update(vector<vector<state_t>> & newWorld, const vector<vector<state_t>> & prevWorld) const override {
+            for (auto i = 0; i < prevWorld.size(); i++) {
+                for (auto j = 0; j < prevWorld[0].size(); j++) {
+                    auto occ = this->countAdj(i, j, OCCUPIED, prevWorld);
+                    if (prevWorld[i][j] == EMPTY and occ == 0) {
+                        newWorld[i][j] = OCCUPIED;
+                    } else if (prevWorld[i][j] == OCCUPIED and occ >= 4) {
+                        newWorld[i][j] = EMPTY;
+                    } else {
+                        newWorld[i][j] = prevWorld[i][j];
+                    }
+                }
+            }
+        }
+    private:
+        int countAdj(int i, int j, state_t state, const vector<vector<state_t>> & world) const
+        {
+            int cnt = 0;
+            for (auto x = i - 1; x <= i + 1; x++) {
+                for (auto y = j - 1; y <= j + 1; y++) {
+                    if (x < 0 or y < 0 or x >= world.size() or y >= world[0].size() or (x == i and y == j)) {
+                        continue;
+                    }
+
+                    cnt += world[x][y] == state ? 1 : 0;
+                }
+            }
+
+            return cnt;
+        }
+
+};
+
+class UpdateRules2 : public UpdateRules {
+    public:
+        void update(vector<vector<state_t>> & newWorld, const vector<vector<state_t>> & prevWorld) const override {
+            for (auto i = 0; i < prevWorld.size(); i++) {
+                for (auto j = 0; j < prevWorld[0].size(); j++) {
+                    auto occ = this->countAdj(i, j, OCCUPIED, prevWorld);
+                    if (prevWorld[i][j] == EMPTY and occ == 0) {
+                        newWorld[i][j] = OCCUPIED;
+                    } else if (prevWorld[i][j] == OCCUPIED and occ >= 5) {
+                        newWorld[i][j] = EMPTY;
+                    } else {
+                        newWorld[i][j] = prevWorld[i][j];
+                    }
+                }
+            }
+        }
+    private:
+        int countAdj(int i, int j, state_t state, const vector<vector<state_t>> & world) const
+        {
+            vector<std::pair<int, int>> directions{ {1, 1}, {0, 1}, {-1, 1}, {1, 0}, {-1, 0}, {1, -1}, {0, -1}, {-1, -1}};
+            int cnt = 0;
+            for(const auto & p : directions) {
+                for (auto x = i, y = j; x >= 0 and y >= 0 and x < world.size() and y < world[0].size(); x += p.first, y += p.second) {
+                    if (x == i and y == j) {
+                        continue;
+                    }
+
+                    if (world[x][y] == state) {
+                        cnt += 1;
+                        break;
+                    } else if (world[x][y] != FLOOR) {
+                        break;
+                    }
+                }
+            }
+
+            return cnt;
+        }
+
+};
+
+
 bool stable(const vector<vector<state_t>> & left, const vector<vector<state_t>> & right);
-void update(vector<vector<state_t>> & newWorld, const vector<vector<state_t>> & prevWorld);
-int countAdj(int i, int j, state_t state, const vector<vector<state_t>> & world);
+void process(vector<vector<state_t>> world, const UpdateRules& update);
 
 void printW(const vector<vector<state_t>> & world)
 {
@@ -47,11 +129,18 @@ int main() {
     }
     input.close();
 
+    process(world, UpdateRules1());
+    process(world, UpdateRules2());
 
+    return 0;
+}
+
+void process(vector<vector<state_t>> world, const UpdateRules& update)
+{
     vector<vector<state_t>> worldCpy;
     do {
         worldCpy = world;
-        update(world, worldCpy);
+        update.update(world, worldCpy);
     } while(not stable(world, worldCpy));
 
     long int cnt = 0;
@@ -63,7 +152,6 @@ int main() {
 
     cout << cnt << " occupied seats" << endl;
 
-    return 0;
 }
 
 bool stable(const vector<vector<state_t>> & left, const vector<vector<state_t>> & right)
@@ -77,37 +165,4 @@ bool stable(const vector<vector<state_t>> & left, const vector<vector<state_t>> 
     }
 
     return true;
-}
-
-void update(vector<vector<state_t>> & newWorld, const vector<vector<state_t>> & prevWorld)
-{
-    for (auto i = 0; i < prevWorld.size(); i++) {
-        for (auto j = 0; j < prevWorld[0].size(); j++) {
-            auto occ = countAdj(i, j, OCCUPIED, prevWorld);
-            if (prevWorld[i][j] == EMPTY and occ == 0) {
-                newWorld[i][j] = OCCUPIED;
-            } else if (prevWorld[i][j] == OCCUPIED and occ >= 4) {
-                newWorld[i][j] = EMPTY;
-            } else {
-                newWorld[i][j] = prevWorld[i][j];
-            }
-        }
-    }
-}
-
-
-int countAdj(int i, int j, state_t state, const vector<vector<state_t>> & world)
-{
-    int cnt = 0;
-    for (auto x = i - 1; x <= i + 1; x++) {
-        for (auto y = j - 1; y <= j + 1; y++) {
-            if (x < 0 or y < 0 or x >= world.size() or y >= world[0].size() or (x == i and y == j)) {
-                continue;
-            }
-
-            cnt += world[x][y] == state ? 1 : 0;
-        }
-    }
-
-    return cnt;
 }
