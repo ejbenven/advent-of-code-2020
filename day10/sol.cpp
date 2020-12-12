5f99d555d658fb5d1a3087e8a9ea38c51188fe68
Eloi Benvenuti <me@eloi.io>
2020-12-12 23:09:48 +0100

    Day 10 Part 2

--- day10/sol.cpp
+++ day10/sol.cpp
@@ -17,7 +17,7 @@ int main() {
     //input.open("day10/test_input.txt");
 
     string line;
-    std::vector<unsigned int> joltages;
+    std::vector<int> joltages;
     while(std::getline(input, line)) {
         joltages.push_back(std::stoul(line));
     }
@@ -27,7 +27,7 @@ int main() {
 
     joltages.insert(joltages.begin(), 0);
     joltages.insert(joltages.end(), joltages.back() + 3);
-    std::unordered_map<unsigned int, unsigned int> counts{ {0, 0}, {1, 0}, {2, 0}, {3, 0}};
+    std::unordered_map<int, int> counts{ {0, 0}, {1, 0}, {2, 0}, {3, 0}};
 
     for (auto i = 0; i < joltages.size() - 1; i++) {
         auto diff = joltages[i + 1] - joltages[i];
@@ -36,6 +36,19 @@ int main() {
     }
 
     cout << counts[1] * counts[3] << endl;
+
+    std::vector<long long int> arrangements(joltages.size(), 0);
+    arrangements[arrangements.size()-1] = 1;
+
+    for (int i = arrangements.size() - 1; i >= 0; i--) {
+        int j = i - 1;
+        while (j >= 0 and joltages[j] >= joltages[i] - 3) {
+            arrangements[j] += arrangements[i];
+            j--;
+        }
+    }
+
+    cout << arrangements[0] << " arrangements in total" << endl;
 
     return 0;
 }
