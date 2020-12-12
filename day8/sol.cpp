db25b9aaa841e4787301485796142f8e953c90f2
Eloi Benvenuti <me@eloi.io>
2020-12-12 01:40:23 +0100

    Day8 Part 2

--- day8/sol.cpp
+++ day8/sol.cpp
@@ -23,16 +23,25 @@ struct operator_var_t {
 class Operator {
     public:
         virtual operator_var_t exec(operator_var_t input)=0;
+        virtual std::pair<bool, operator_var_t> execOther(operator_var_t input)=0;
         virtual ~Operator() = default;
 };
 
 class Nop : public Operator {
     public:
+        Nop (int val) { this->val = val; }
         operator_var_t exec(operator_var_t input) override
         {
             input.line += 1;
             return input;
         }
+        std::pair<bool, operator_var_t> execOther(operator_var_t input) override
+        {
+            input.line += this->val;
+            return std::make_pair(true, input);
+        }
+    private:
+        int val;
 };
 
 class Acc : public Operator {
@@ -43,6 +52,11 @@ class Acc : public Operator {
             input.acc += this->acc;
             input.line += 1;
             return input;
+        }
+
+        std::pair<bool, operator_var_t> execOther(operator_var_t input) override
+        {
+            return std::make_pair(false, input);
         }
     private:
         int acc;
@@ -56,6 +70,12 @@ class Jmp : public Operator {
             input.line += this->jmp;
             return input;
         }
+
+        std::pair<bool, operator_var_t> execOther(operator_var_t input) override
+        {
+            input.line += 1;
+            return std::make_pair(true, input);
+        }
     private:
         int jmp;
 };
@@ -64,6 +84,8 @@ class Jmp : public Operator {
 
 
 unique_ptr<Operator> parse(string line);
+std::pair<bool, operator_var_t> bugfix(const vector<unique_ptr<Operator>>& program,
+        operator_var_t ptr, unordered_set<unsigned int>& visited, bool switched);
 
 int main() {
     std::ifstream input;
@@ -75,6 +97,7 @@ int main() {
     while(std::getline(input, line)) {
         program.push_back(parse(line));
     }
+    input.close();
 
     operator_var_t ptr = {0, 0};
     unordered_set<unsigned int> visited;
@@ -86,25 +109,60 @@ int main() {
 
     cout << "Acc value = " << ptr.acc << endl;
 
-    input.close();
+    visited.clear();
+    ptr = { 0, 0 };
+    auto ans = bugfix(program, ptr, visited, false);
+
+    if (not ans.first) {
+        cout << "No solution was found" << endl;
+    } else {
+        cout << "Accumulator value at end of program is: " << ans.second.acc << endl;
+    }
 
 
     return 0;
+}
+
+std::pair<bool, operator_var_t> bugfix(const vector<unique_ptr<Operator>>& program,
+        operator_var_t ptr, unordered_set<unsigned int>& visited, bool switched)
+{
+    if (ptr.line < 0 or ptr.line > program.size() + 1 or visited.contains(ptr.line)) {
+        return std::make_pair(false, ptr);
+    } else if (ptr.line == program.size()) {
+        return std::make_pair(true, ptr);
+    }
+
+    visited.insert(ptr.line);
+    auto newPtr = program[ptr.line]->exec(ptr);
+    auto res = bugfix(program, newPtr, visited, switched);
+
+    if (res.first or switched) {
+        return res;
+    } else {
+        auto fix = program[ptr.line]->execOther(ptr);
+
+        if (not fix.first) {
+            visited.erase(ptr.line);
+            return res;
+        } else {
+            res = bugfix(program, fix.second, visited, true);
+            visited.erase(ptr.line);
+            return res;
+        }
+    }
 }
 
 unique_ptr<Operator> parse(string line)
 {
     vector<absl::string_view> args = absl::StrSplit(line, " ");
     unique_ptr<Operator> ans;
+    int arg;
+    (void)absl::SimpleAtoi(args[1], &arg);
     if(args[0] == "nop") {
-        ans = std::make_unique<Nop>();
+        ans = std::make_unique<Nop>(arg);
     } else if (args[0] == "acc") {
-        int arg;
-        (void)absl::SimpleAtoi(args[1], &arg);
         ans = std::make_unique<Acc>(arg);
     } else {
-        int arg;
-        (void)absl::SimpleAtoi(args[1], &arg);
         ans = std::make_unique<Jmp>(arg);
     }
 
